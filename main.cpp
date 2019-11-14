c2ee20bf91b4e4f8911bc2f973fa09f4614a1508
evanhr18 <evanhr18@wfu.edu>
2019-11-14 15:11:37 -0500

    i can print the list. haven't done the first initial thing yet.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 int main() {
     ifstream inFile;
-    inFile.open ("Students");
+    inFile.open ("../Students");
+    string name;
+    vector <string> names;
+    while (!inFile.eof()) {
+        getline(inFile, name,',');
+        if (!name.empty()) {
+            names.push_back(name);
+        }
+    }
+    for (string n : names) {
+        cout << n;
+    }
+
 
     return 0;
 }
